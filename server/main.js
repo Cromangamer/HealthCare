c81435ea9d50e243d45d5461c094f7966edc6d5d
Pankhaniya Dhruv <vijyapankhaniya1968@gmail.com>
2026-07-11 03:35:05 +0530

    This is my first Set-UP face of this Whole Project

---
+++ server/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
